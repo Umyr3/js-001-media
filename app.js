3921f76213426cfbd820d9fdfde8c964b42cc410
Luka <lukaryan.btw@gmail.com>
2025-05-09 22:29:20 -0300

    Mostrar aprovado ou reprovado

--- app.js
+++ app.js
@@ -1,3 +1,8 @@
 let n1 = 7, n2 = 5.5, n3 = 2, n4 = 4
 let media = (n1 + n2 + n3 + n4) / 4
 console.log("A media é: " + media.toFixed(2))
+if (media<= 5) {
+   console.log ("você foi reprovado")
+} else {
+    console.log ("Você foi aprovado!!!")
+}
